acbac70d7091785a14dbc4347bd0d2774394822d
Brian Leathem <bleathem@gmail.com>
2015-09-28 13:47:59 -0700

    Refactored canvasDrawr into a separate file

--- appform.js
+++ appform.js
@@ -2,6 +2,7 @@
 
 var angular = require('angular');
 var _ = require('lodash');
+var canvasDrawr = require('./canvas-drawr');
 
 var ngModule = angular.module('wfm.appform', ['wfm.core.mediator']);
 
@@ -235,154 +236,6 @@ ngModule.directive('appformFieldNumber', function($templateCache, $window, $docu
 });
 
 ngModule.directive('appformFieldSignature', function($templateCache, $window, $document, mediator) {
-
-  var CanvasDrawrMouse = function (element, options) {
-    var canvas = element[0].getElementsByTagName('canvas')[0];
-    var ctx = canvas.getContext("2d");
-    canvas.style.width = '100%';
-    canvas.width = (window.innerWidth);
-    canvas.height = 200;
-    canvas.style.width = '';
-
-    // set props from options, but the defaults are for the cool kids
-    ctx.lineWidth = options.size || 5;
-    ctx.lineCap = options.lineCap || "round";
-    options.color = options.color || 'blue';
-
-    // last known position
-    var pos = { x: 0, y: 0 };
-
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mousedown', setPosition);
-
-    // new position from mouse event
-    function setPosition(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      var rect = canvas.getBoundingClientRect();
-      var offset = {
-        top: rect.top,
-        left: rect.left
-      };
-      pos.x = e.clientX - offset.left;
-      pos.y = e.clientY - offset.top;
-    }
-
-    function draw(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      // mouse left button must be pressed
-      if (e.buttons !== 1) return;
-
-      ctx.beginPath(); // begin
-
-      ctx.strokeStyle = options.color;
-
-      ctx.moveTo(pos.x, pos.y); // from
-
-      var rect = canvas.getBoundingClientRect();
-      var offset = {
-        top: rect.top,
-        left: rect.left
-      };
-      pos.x = e.clientX - offset.left;
-      pos.y = e.clientY - offset.top;
-      ctx.lineTo(pos.x, pos.y); // to
-
-      ctx.stroke(); // draw it!
-
-    }
-  };
-
-  var CanvasDrawr = function(element, options) {
-    var canvas = element[0].getElementsByTagName('canvas')[0];
-    var ctx = canvas.getContext('2d');
-    canvas.style.width = '100%'
-    canvas.width = canvas.offsetWidth;
-    canvas.style.width = '';
-
-    // set props from options, but the defaults are for the cool kids
-    ctx.lineWidth = options.size || 5;
-    ctx.lineCap = options.lineCap || 'round';
-    options.color = options.color || 'blue';
-    ctx.pX = undefined;
-    ctx.pY = undefined;
-
-    var lines = [,,];
-    var rect = canvas.getBoundingClientRect();
-
-    var offset = {
-      top: rect.top + $document[0].body.scrollTop,
-      left: rect.left + $document[0].body.scrollLeft
-    };
-
-    var self = {
-      //bind click events
-      init: function() {
-        // use anguler.element#on for automatic listener cleanup
-        var canvasNg = angular.element(canvas);
-        //set pX and pY from first click
-        canvasNg.on('touchstart', self.preDraw);
-        canvasNg.on('touchmove', self.draw);
-        canvasNg.on('touchend', function(event) {
-          $ionicScrollDelegate.freezeAllScrolls(false);
-        });
-      },
-
-      preDraw: function(event) {
-        $ionicScrollDelegate.freezeAllScrolls(true);
-        rect = canvas.getBoundingClientRect();
-        offset = {
-          top: rect.top + $document[0].body.scrollTop,
-          left: rect.left + $document[0].body.scrollLeft
-        };
-
-        for (var i = 0; i < event.touches.length; i++) {
-          var touch = event.touches[i];
-          var id      = touch.identifier;
-
-          lines[id] = {
-            x     : touch.pageX - offset.left,
-            y     : touch.pageY - offset.top,
-            color : options.color
-          };
-        };
-        event.preventDefault();
-        event.cancelBubble = true;
-      },
-
-      draw: function(event) {
-        for (var i = 0; i < event.touches.length; i++) {
-          var touch = event.touches[i];
-          var id = touch.identifier,
-
-          moveX = touch.pageX - offset.left - lines[id].x,
-          moveY = touch.pageY - offset.top - lines[id].y;
-
-          var ret = self.move(id, moveX, moveY);
-          lines[id].x = ret.x;
-          lines[id].y = ret.y;
-        };
-
-        event.preventDefault();
-      },
-
-      move: function(i, changeX, changeY) {
-        ctx.strokeStyle = lines[i].color;
-        ctx.beginPath();
-        ctx.moveTo(lines[i].x, lines[i].y);
-
-        ctx.lineTo(lines[i].x + changeX, lines[i].y + changeY);
-        ctx.stroke();
-        ctx.closePath();
-
-        return { x: lines[i].x + changeX, y: lines[i].y + changeY };
-      }
-    };
-
-    return self.init();
-  }
-
   return {
     restrict: 'E'
   , template: '<div class="appform-signature-field"><canvas></canvas></div>'
@@ -391,7 +244,7 @@ ngModule.directive('appformFieldSignature', function($templateCache, $window, $d
     }
   , link: function (scope, element, attrs) {
       var options = scope.options || {};
-      var drawr = new CanvasDrawrMouse(element, options);
+      var drawr = new canvasDrawr.CanvasDrawrMouse(element, options);
     }
   };
 })
