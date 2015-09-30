b80f3ef4c93724bf1326f97acc31a9a7568c9f65
Brian Leathem <bleathem@gmail.com>
2015-09-30 06:45:12 -0700

    Fixed the $fh.forms cloud integration

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 'use strict';
 
-var $fh = require('fh-mbaas-api');
-
 var events = require('events');
-var submissionEventListener = new events.EventEmitter();
 
-$fh.forms.registerListener(submissionEventListener, function(err){
-  if (err) {
-    console.err(err);
-  };
+module.exports = function($fh) {
 
-  //submissionEventListener has now been registered with the $fh.forms Cloud API. Any valid Forms Events will now emit.
-});
+  var submissionEventListener = new events.EventEmitter();
 
-submissionEventListener.on('submissionStarted', function(params){
-  var submissionId = params.submissionId;
-  var submissionStartedTimestamp = params.submissionStartedTimestamp;
-  console.log("Submission with ID " + submissionId + " has started at " + submissionStartedTimestamp);
-});
+  submissionEventListener.on('submissionStarted', function(params){
+    var submissionId = params.submissionId;
+    var submissionStartedTimestamp = params.submissionStartedTimestamp;
+    console.log('****************************************');
+    console.log('params', params);
+    console.log('Submission with ID ' + submissionId + ' has started at ' + submissionStartedTimestamp);
+    console.log('****************************************');
+  });
 
-submissionEventListener.on('submissionComplete', function(params){
-  var submissionId = params.submissionId;
-  var submissionCompletedTimestamp = params.submissionCompletedTimestamp;
-  console.log("Submission with ID " + submissionId + " has completed at " + submissionCompletedTimestamp);
-});
+  submissionEventListener.on('submissionComplete', function(params){
+    var submissionId = params.formSubmission.formSubmittedAgainst._id; //params.submissionId; //FH-2213
+    var submissionCompletedTimestamp = params.formSubmission.submissionCompletedTimestamp; //params.submissionCompletedTimestamp; //FH-2213
+    console.log('****************************************');
+    console.log('params', params);
+    console.log('Submission with ID ' + submissionId + ' has completed at ' + submissionCompletedTimestamp);
+    console.log('****************************************');
+  });
+
+  $fh.forms.registerListener(submissionEventListener, function(err){
+    console.log('****************************************');
+    if (err) {
+      console.err(err);
+    } else {
+      console.log('submissionEventListener has now been registered with the $fh.forms Cloud API. Any valid Forms Events will now emit.');
+    };
+    console.log('****************************************');
+  });
+};
