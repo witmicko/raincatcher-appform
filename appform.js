01120101e9d15124c72c5d6dcae89771debb2c76
Brian Leathem <bleathem@gmail.com>
2015-11-10 17:27:23 -0800

    Changed the appform directive implementations to use the appform-client angular service wrapper, rather than the mediator api.

--- appform.js
+++ appform.js
@@ -5,4 +5,8 @@ module.exports = 'wfm.appform';
 angular.module('wfm.appform', [
   'wfm.core.mediator'
 , require('./lib/ng-modules/directives')
-]);
+])
+
+.run(function(mediator) {
+  require('./lib/wrappers/mediator-client')(mediator);
+});
